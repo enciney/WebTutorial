3f1717354bfcdaad90b41a4a8de8bec5922dd956
enciney <enginyldrm02@gmail.com>
2017-06-20 01:48:02 +0300

    angular Code Files.

---
+++ Lecture4-Angular/appJS.js
@@ -0,0 +1,18 @@
+// my module name and also  dependencies between the scopes
+(function(){
+
+'use strict';
+// when we use 'use strict'  we mean that check the global variables definition
+// if you ran the code as a delete the var from the 'value' variable compile said that
+// value is not defined.
+var value = "Smth";
+
+
+
+angular.module('AngularApp',[])
+
+.controller('firstController',function(){
+
+});
+
+})();
