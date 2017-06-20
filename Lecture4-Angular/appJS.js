30b06482233b543940121de0a903102e42cefce0
enciney <enginyldrm02@gmail.com>
2017-06-20 20:27:44 +0300

    Angular Basic Codes

--- Lecture4-Angular/appJS.js
+++ Lecture4-Angular/appJS.js
@@ -10,8 +10,32 @@ var value = "Smth";
 
 
 angular.module('AngularApp',[])
+// if delete the $scope you can see that our variables will not be binded with wiew html
+.controller('firstController',function($scope){
+  $scope.name = '';
+  $scope.numValue = 0;
+  $scope.saySmth = function(){
 
-.controller('firstController',function(){
+      return 'Hi Everyone';
+
+  }
+  $scope.displayNumeric = function(){
+      var numOfNumeric = calculateNumeric($scope.name) ;
+      $scope.numValue = numOfNumeric ;
+
+  }
+
+  function calculateNumeric(string)
+  {
+    var totalNameValue = 0 ;
+    for (var i = 0; i < string.length; i++) {
+      totalNameValue += string.charCodeAt(i);
+    }
+    return totalNameValue;
+
+  }
+
+
 
 });
 
